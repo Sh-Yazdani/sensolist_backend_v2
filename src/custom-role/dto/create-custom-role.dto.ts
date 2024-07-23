0275382e2bce6e86090e0dfa11a6119917e7cf69
MostafaGhanbari9176 <godhelot1@outlook.com>
2024-07-23 15:17:42 +0330

    feat: adding validation decorators

--- src/custom-role/dto/create-custom-role.dto.ts
+++ src/custom-role/dto/create-custom-role.dto.ts
@@ -1,10 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger"
+import { IsString } from "class-validator"
 
 export class CreateCustomRoleDto {
+    
     @ApiProperty({ type: String })
+    @IsString()
     name: string
 
     @ApiProperty({ type: String })
+    @IsString()
     description: string
 }
 
