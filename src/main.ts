c944320fbbf498d97e11f1bf50a4603a7194366c
MostafaGhanbari9176 <godhelot1@outlook.com>
2024-07-24 15:19:07 +0330

    feat: adding swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+function initializeSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle("Sensolist REST-API documentaion")
+    .setDescription("in case of any confusion feel free for asking")
+    .setVersion("1")
+    .build()
+
+  const documentation = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, documentation)
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  initializeSwagger(app)
+
+  await app.listen(3000, () => {
+    console.log("server listen on port 3000, docs url: ~/docs")
+  });
 }
 bootstrap();
+
+
