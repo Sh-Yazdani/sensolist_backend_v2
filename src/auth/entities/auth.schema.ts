9aab3876cdf6dbcea01eed734957629063b09950
MostafaGhanbari9176 <godhelot1@outlook.com>
2024-07-25 19:37:56 +0330

    fix:fixing auth schema

--- src/auth/entities/auth.schema.ts
+++ src/auth/entities/auth.schema.ts
@@ -1,13 +1,18 @@
-import { SchemaFactory } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 
+@Schema({timestamps:true})
 export class OTP {
 
+    @Prop({type:String})
     phonenumber: string
 
+    @Prop({type:String})
     otp: string
 
+    @Prop({type:String})
     token: string
 
+    @Prop({type:String})
     date: Date
 
 }
