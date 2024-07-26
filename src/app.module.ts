cc188d03bdcc77edaf5130b213e9a041e1575657
MostafaGhanbari9176 <godhelot1@outlook.com>
2024-07-26 16:19:09 +0330

    fix:serving swagger static resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,14 @@ import { UserModule } from './user/user.module';
 import { ThingsModule } from './things/things.module';
 import configuration from './config/configuration';
 import { AuthModule } from './auth/auth.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath:join(__dirname, 'node_modules', 'swagger-ui-dist')
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
