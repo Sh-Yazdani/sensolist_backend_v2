a15758aeb73319d2ec3678b76b941da3d5b4ec34
MostafaGhanbari9176 <godhelot1@outlook.com>
2024-07-26 14:05:36 +0330

    fix:changing db connection

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,7 @@
 export default () => ({
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://root:sb2D8ByEtDRvGyiJ@cluster0.ispo7zw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   MONGODB_NAME: process.env.MONGODB_NAME || 'sensolist_DB',
   MONGODB_USER: process.env.MONGODB_USER || 'root',
-  MONGODB_PASS: process.env.MONGODB_PASS || '1234',
+  MONGODB_PASS: process.env.MONGODB_PASS || 'sb2D8ByEtDRvGyiJ',
 });
 
