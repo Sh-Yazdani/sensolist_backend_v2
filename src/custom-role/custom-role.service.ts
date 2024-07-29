9a3c0eea8f77330161ed7e0bdd28150342fb3750
MostafaGhanbari9176 <godhelot1@outlook.com>
2024-07-29 13:20:03 +0330

    fix: fixing custom role service

--- src/custom-role/custom-role.service.ts
+++ src/custom-role/custom-role.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCustomRoleDto } from './dto/create-custom-role.dto';
 import { UpdateCustomRoleDto } from './dto/update-custom-role.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { CustomRole } from './entities/custom-role.entity';
-import { Model, ObjectId } from 'mongoose';
+import { Model, ObjectId, Types } from 'mongoose';
 import { MessageResponseDTO } from '../dto/response.dto';
 import { CustomRoleListResponseDTO } from './dto/custom-role-list.dto';
 import { CustomRoleEntity, CustomRoleEntityResponseDTO } from './dto/custom-role-entity.dto';
@@ -26,7 +26,7 @@ export class CustomRoleService {
     const roles = await this.customRoleModel.find().exec()
 
     return {
-      list: roles.map(r => ({ id:r._id, name: r.name as string, description: r.description as string })),
+      list: roles.map(r => ({ id: r._id, name: r.name as string, description: r.description as string })),
       statusCode: 200
     }
   }
@@ -34,14 +34,17 @@ export class CustomRoleService {
   async findOne(id: ObjectId): Promise<CustomRoleEntityResponseDTO> {
     const role = await this.customRoleModel.findById(id)
 
+    if(!role)
+      throw new NotFoundException("role is not exists")
+
     return {
       statusCode: 200,
-      role: {id:role._id, name: role.name as string, description: role.description as string }
+      role: { id: role._id, name: role.name as string, description: role.description as string }
     }
   }
 
   async update(id: ObjectId, data: UpdateCustomRoleDto): Promise<MessageResponseDTO> {
-    await this.customRoleModel.updateOne({ _id: id }, { data })
+    await this.customRoleModel.updateOne({ _id: id }, { ...data }).exec()
 
     return {
       statusCode: 200,
@@ -58,7 +61,7 @@ export class CustomRoleService {
     }
   }
 
-  async getRoles(){
+  async getRoles() {
     return await this.customRoleModel.find().exec()
   }
 
