0e52790cfa424275177e2c893663486677545edf
MostafaGhanbari9176 <godhelot1@outlook.com>
2024-07-30 21:35:16 +0330

    refacttor: improving thing controller

--- src/things/things.controller.ts
+++ src/things/things.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, NotFoundException } from '@nestjs/common';
 import { ThingsService } from './things.service';
 import { CreateThingDto } from './dto/create-thing.dto';
 import { UpdateThingDto } from './dto/update-thing.dto';
@@ -22,16 +22,26 @@ export class ThingsController {
   @ApiOperation({ summary: "creating a new thing" })
   @ApiCreatedResponse({ type: MessageResponseDTO })
   @ApiInternalServerErrorResponse({ type: ErrorResponseDTO })
-  create(@Body() createThingDto: CreateThingDto) {
-    return this.thingsService.create(createThingDto);
+  async create(@Body() createThingDto: CreateThingDto): Promise<MessageResponseDTO> {
+    await this.thingsService.create(createThingDto);
+
+    return {
+      statusCode: 201,
+      message: "thing was created"
+    }
   }
 
   @Get()
   @ApiOperation({ summary: "list of all things", description: "with this API you can search and filter things, for getting all exists things, dont pass the search and filter params" })
   @ApiOkResponse({ type: ThingListResponseDTO })
   @ApiInternalServerErrorResponse({ type: ErrorResponseDTO })
-  findAll(@Query() query: ThingQueryDTO) {
-    return this.thingsService.findAll(query);
+  async findAll(@Query() query: ThingQueryDTO): Promise<ThingListResponseDTO> {
+    const things = await this.thingsService.findAll(query);
+
+    return {
+      statusCode: 200,
+      list: things
+    }
   }
 
   @Get(':id')
@@ -40,8 +50,16 @@ export class ThingsController {
   @ApiParam({ name: "id", type: String, description: "the thing id" })
   @ApiInternalServerErrorResponse({ type: ErrorResponseDTO })
   @ApiNotFoundResponse({ type: ErrorResponseDTO })
-  findOne(@Param('id') id: ObjectId) {
-    return this.thingsService.findOne(id);
+  async findOne(@Param('id') id: ObjectId): Promise<ThingEntityResponseDTO> {
+    const thing = await this.thingsService.findOne(id);
+
+    if (!thing)
+      throw new NotFoundException("thing is not exists")
+
+    return {
+      statusCode: 200,
+      thing: thing
+    }
   }
 
   @Patch(':id')
@@ -50,8 +68,16 @@ export class ThingsController {
   @ApiParam({ name: "id", type: String, description: "the thing id" })
   @ApiNotFoundResponse({ type: ErrorResponseDTO })
   @ApiInternalServerErrorResponse({ type: ErrorResponseDTO })
-  update(@Param('id') id: ObjectId, @Body() updateThingDto: UpdateThingDto) {
-    return this.thingsService.update(id, updateThingDto);
+  async update(@Param('id') id: ObjectId, @Body() updateThingDto: UpdateThingDto) {
+    const updated = await this.thingsService.update(id, updateThingDto);
+
+    if (!updated)
+      throw new NotFoundException("thing not found")
+
+    return {
+      statusCode: 200,
+      message: "user was updated"
+    }
   }
 
   @Delete(':id')
@@ -59,7 +85,12 @@ export class ThingsController {
   @ApiOkResponse({ type: MessageResponseDTO })
   @ApiParam({ name: "id", type: String, description: "the thing id" })
   @ApiInternalServerErrorResponse({ type: ErrorResponseDTO })
-  remove(@Param('id') id: ObjectId) {
-    return this.thingsService.remove(id);
+  async remove(@Param('id') id: ObjectId): Promise<MessageResponseDTO> {
+    await this.thingsService.remove(id);
+
+    return {
+      statusCode: 200,
+      message: "user was deleted"
+    }
   }
 }
--- src/things/things.service.ts
+++ src/things/things.service.ts
@@ -7,23 +7,18 @@ import { Model, ObjectId } from 'mongoose';
 import { ThingQueryDTO, ThingSortOptions } from './dto/thing-search.dto';
 import { MessageResponseDTO } from '../dto/response.dto';
 import { ThingListResponseDTO } from './dto/thing.list.dto';
-import { ThingEntityResponseDTO } from './dto/thing-entity.dto';
+import { ThingEntityDTO, ThingEntityResponseDTO } from './dto/thing-entity.dto';
 
 @Injectable()
 export class ThingsService {
 
   constructor(@InjectModel(Thing.name) private readonly thingModel: Model<Thing>) { }
 
-  async create(data: CreateThingDto): Promise<MessageResponseDTO> {
-    const newThing = await this.thingModel.create(data)
-
-    return {
-      statusCode: 201,
-      message: "thing was created"
-    }
+  async create(data: CreateThingDto): Promise<void> {
+    await this.thingModel.create(data)
   }
 
-  async findAll(query: ThingQueryDTO): Promise<ThingListResponseDTO> {
+  async findAll(query: ThingQueryDTO): Promise<ThingEntityDTO[]> {
 
     let thingsDBQuery = this.thingModel.find()
 
@@ -34,7 +29,7 @@ export class ThingsService {
     const search: string = query.search ?? ""
 
     thingsDBQuery = thingsDBQuery.find({
-      name: new RegExp(query.search, 'i'),
+      name: new RegExp(search, 'i'),
       actions: { $in: actions },
       brand: { $in: brand },
       type: { $in: type },
@@ -53,71 +48,58 @@ export class ThingsService {
     const things = await thingsDBQuery.exec()
 
 
-    return {
-      statusCode: 200,
-      list: things.map(t => {
-        return {
-          id: t._id,
-          name: t.name,
-          brand: t.brand,
-          model: t.model,
-          type: t.type,
-          actions: t.actions,
-          characteristics: t.characteristics,
-          activition: t.activition,
-          description: t.description,
-          images: t.images
-        }
-      })
-    }
+    return things.map(t => {
+      return {
+        id: t._id,
+        name: t.name,
+        brand: t.brand,
+        model: t.model,
+        type: t.type,
+        actions: t.actions,
+        characteristics: t.characteristics,
+        activition: t.activition,
+        description: t.description,
+        images: t.images
+      }
+    })
+
   }
 
-  async findOne(id: ObjectId): Promise<ThingEntityResponseDTO> {
+  async findOne(id: ObjectId): Promise<ThingEntityDTO | undefined> {
     const thing = await this.thingModel.findById(id).exec()
 
-    if (!thing)
-      throw new NotFoundException("thing is not exists")
+    if(!thing)
+      return undefined
 
     const coverImage = thing.images?.find(i => i.isCover)
 
     return {
-      statusCode: 200,
-      thing: {
-        id: thing._id,
-        name: thing.name,
-        brand: thing.brand,
-        model: thing.model,
-        type: thing.type,
-        actions: thing.actions,
-        characteristics: thing.characteristics,
-        activition: thing.activition,
-        description: thing.description,
-        images: coverImage ? [coverImage] : []
-      }
+      id: thing._id,
+      name: thing.name,
+      brand: thing.brand,
+      model: thing.model,
+      type: thing.type,
+      actions: thing.actions,
+      characteristics: thing.characteristics,
+      activition: thing.activition,
+      description: thing.description,
+      images: coverImage ? [coverImage] : []
     }
   }
 
-  async update(id: ObjectId, data: UpdateThingDto): Promise<MessageResponseDTO> {
+  async update(id: ObjectId, data: UpdateThingDto): Promise<boolean> {
     const thing = await this.thingModel.findById(id).exec()
 
-    if (thing == undefined)
-      throw new NotFoundException("id is not found")
+    if(!thing)
+      return false
 
     await thing.updateOne({ ...data }).exec()
 
-    return {
-      statusCode: 200,
-      message: "user was updated"
-    }
+    return true
   }
 
-  async remove(id: ObjectId) {
+  async remove(id: ObjectId):Promise<void> {
     await this.thingModel.deleteOne({ _id: id }).exec()
-
-    return {
-      status: 200,
-      message: "user was deleted"
-    }
   }
 
 }
