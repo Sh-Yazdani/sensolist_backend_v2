645ef32d6c82bc535cc1e7d0fc7ea9bdcf3e1038
MostafaGhanbari9176 <godhelot1@outlook.com>
2024-08-03 20:54:21 +0330

    feat: implementing dashboard seeder

--- src/dashboard/entities/dashboard.entity.ts
+++ src/dashboard/entities/dashboard.entity.ts
@@ -1,23 +1,29 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { ObjectId, Types } from "mongoose";
+import { Factory } from "nestjs-seeder";
 
 @Schema({
     timestamps: true
 })
 export class Dashboard {
 
+    @Factory(faker => faker.company.name())
     @Prop({ type: String, require: true })
     name: string
 
+    @Factory(faker => faker.lorem.paragraph({ min: 1, max: 5 }))
     @Prop({ type: String, require: true })
     description: string
 
+    @Factory([])
     @Prop({ type: [Types.ObjectId], require: true })
     assignedUsers: Types.ObjectId[]
 
+    @Factory("")
     @Prop({ type: Types.ObjectId, require: true })
     imageId: Types.ObjectId
 
+    @Factory(false)
     @Prop({ type: Boolean, require: true, default: false })
     pinned: boolean
 }
