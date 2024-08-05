b0fc4af8a4917c55ff7a29dbe4734663eefaacc3
MostafaGhanbari9176 <godhelot1@outlook.com>
2024-08-05 21:10:35 +0330

    docs:completing widget docs

--- src/widget/widget.controller.ts
+++ src/widget/widget.controller.ts
@@ -2,14 +2,23 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException 
 import { WidgetService } from './widget.service';
 import { RawWidgetGroupesResponseDTO } from './dto/widget-list.dto';
 import { WidgetConfigDTO } from './dto/widget-config.dto';
-import { MessageResponseDTO } from 'src/dto/response.dto';
+import { ErrorResponseDTO } from 'src/dto/response.dto';
 import { ConfigWidgetResponseDTO } from './dto/config-widget.dto';
+import { ApiBadRequestResponse, ApiBearerAuth, ApiCreatedResponse, ApiInternalServerErrorResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CheckSystemRole } from 'src/decorator/role.decorator';
+import { SystemRoles } from 'src/enums/role.enum';
 
 @Controller('widget')
+@ApiTags("widget")
+@CheckSystemRole([SystemRoles.Admin, SystemRoles.NonAdmin])
+@ApiBearerAuth("access_token")
 export class WidgetController {
   constructor(private readonly widgetService: WidgetService) { }
 
   @Get("groupes")
+  @ApiOperation({ summary: "a grouped list of all widgets", description: "this api return all widget groups with their widgets" })
+  @ApiOkResponse({ type: RawWidgetGroupesResponseDTO })
+  @ApiInternalServerErrorResponse({ type: ErrorResponseDTO })
   async widgteListWithGroupes(): Promise<RawWidgetGroupesResponseDTO> {
     const groupes = await this.widgetService.widgteListWithGroupes()
 
@@ -20,6 +29,10 @@ export class WidgetController {
   }
 
   @Post("config")
+  @ApiOperation({ summary: "storing widget config", description: "when user adding a widget to a dashboard you should use this api for storing widget config that user insert, then this api retirn a id you should send this id`s to `dashboard/update` api" })
+  @ApiCreatedResponse({ type: ConfigWidgetResponseDTO })
+  @ApiInternalServerErrorResponse({ type: ErrorResponseDTO })
+  @ApiBadRequestResponse({ type: ErrorResponseDTO })
   async storeWidgetConfig(@Body() data: WidgetConfigDTO): Promise<ConfigWidgetResponseDTO> {
     const configId = await this.widgetService.storeWidgetConfig(data)
 
