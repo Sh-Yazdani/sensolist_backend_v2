32093ef916d8bd85fde7039a286881b45130d2e0
MostafaGhanbari9176 <godhelot1@outlook.com>
2024-08-05 20:22:52 +0330

    feat: implementing widget seeder

---
+++ src/widget/widget.seeder.ts
@@ -0,0 +1,95 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { DataFactory, Seeder } from "nestjs-seeder";
+import { WidgetGroup } from "./entities/widget-group.entity";
+import { Model } from "mongoose";
+import { RawWidget, WidgetFieldsUnits } from "./entities/raw-widget.entity";
+
+@Injectable()
+export class WidgetSeeder implements Seeder {
+
+    groupes = [
+        "Indoor",
+        "Outdoor",
+        "Charts"
+    ]
+
+    widgets = [
+        {
+            name: "time series chart",
+            widgetGroup: this.groupes[2],
+            fields: [
+                {
+                    name: "label",
+                    groupLabel: "X Axes",
+                    description: "",
+                    type: String
+                },
+                {
+                    name: "label",
+                    groupLabel: "Y Axes",
+                    description: "",
+                    type: String
+                },
+                {
+                    name: "min",
+                    groupLabel: "Y Axes",
+                    description: "",
+                    type: Number
+                },
+                {
+                    name: "max",
+                    groupLabel: "Y Axes",
+                    description: "",
+                    type: Number
+                },
+                {
+                    name: "unit",
+                    groupLabel: "Y Axes",
+                    description: "",
+                    type: WidgetFieldsUnits
+                },
+                {
+                    name: "description",
+                    description: "",
+                    type: String
+                },
+            ]
+        }
+    ]
+
+    constructor(
+        @InjectModel(WidgetGroup.name) private readonly groupModel: Model<WidgetGroup>,
+        @InjectModel(RawWidget.name) private readonly widgetModel: Model<RawWidget>,
+    ) { }
+
+    async seed(): Promise<any> {
+
+        let groupes = DataFactory.createForClass(WidgetGroup).generate(3)
+        groupes = groupes.map((g, index) => {
+            return {
+                ...g,
+                name: groupes[index]
+            }
+        })
+        groupes = await this.groupModel.insertMany(groupes)
+
+        let _widgets = DataFactory.createForClass(RawWidget).generate(2)
+        _widgets = _widgets.map((w, index) => {
+            return {
+                ...w,
+                name: this.widgets[0].name,
+                fields: this.widgets[0].fields,
+                widgetGroup: groupes[2]._id
+            }
+        })
+        return this.widgetModel.insertMany(_widgets)
+
+    }
+    async drop(): Promise<any> {
+        await this.widgetModel.deleteMany()
+        return this.groupModel.deleteMany()
+    }
+
+}
+
